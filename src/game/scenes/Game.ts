f0e24af4d7ee2ec86024e1a77b299e76f078a0f3
Felipeacs05 <felipedocarmo05@gmail.com>
2026-05-28 17:49:13 +0100

    Criando chão e plataformas (Static Group)

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -13,7 +13,31 @@ export class Game extends Scene
 
     create ()
     {
+        // Limpar a cor de fundo para um cinzento de ginásio
+        this.cameras.main.setBackgroundColor('#2c3e50');
+
+    
+        // 2. CRIAÇÃO DO CHÃO E PLATAFORMAS (Static Group)
+
         
+        // Objetos que não saem do sítio e servem de barreira
+        this.plataformas = this.physics.add.staticGroup();
+
+        // Vamos criar uma textura temporária para o chão por código (um bloco verde)
+        // createRectangle(x, y, largura, altura, cor)
+        const chãoGrafico = this.make.graphics({ x: 0, y: 0 }).fillStyle(0x27ae60).fillRect(0, 0, 1024, 40);
+        chãoGrafico.generateTexture('chão_temp', 1024, 40);
+
+        // Blocos cinzentos como caixas de Crossfit
+        const platGrafica = this.make.graphics({ x: 0, y: 0 }).fillStyle(0x7f8c8d).fillRect(0, 0, 200, 30);
+        platGrafica.generateTexture('plat_temp', 200, 30);
+
+        // Posicionar o Chão principal no fundo do ecrã (X: 512 [meio], Y: 748 [perto do fundo])
+        this.plataformas.create(512, 748, 'chão_temp');
+
+        // Posicionar duas plataformas aéreas para o Pou poder subir e saltar
+        this.plataformas.create(300, 550, 'plat_temp');
+        this.plataformas.create(724, 420, 'plat_temp');
     }
 
     update ()
